08534c0c2a566cec7b3f4f1139fe36953966c902
岳克奎 <738816656@qq.com>
2017-01-20 11:44:03 +0800

    iOS公有代码
    不管是工具类。。类别 扩展，，平时应该总结下。。在架构里整体作为扩展层

---
+++ STCommon/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  STCommon
+//
+//  Created by GuoMs on 17/1/20.
+//  Copyright © 2017年 ST. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ STCommon/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  STCommon
+//
+//  Created by GuoMs on 17/1/20.
+//  Copyright © 2017年 ST. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
